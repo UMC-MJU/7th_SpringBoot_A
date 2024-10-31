3587495b0d6018c40c65d2188fc7507e82b6365f
SeungGyu <phillipskl123@gmail.com>
2024-10-31 18:41:57 +0900

    [feat] 엔티티 구조 구현

--- 한-한승규/chapter-5/src/main/java/com/umcpractice/chapter_5/common/BaseEntity.java
+++ 한-한승규/chapter-5/src/main/java/com/umcpractice/chapter_5/common/BaseEntity.java
@@ -16,10 +16,10 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name = "updated_at")
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
